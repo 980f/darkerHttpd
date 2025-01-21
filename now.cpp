0d65636cec9737a0aae908e701781a8ea9898195
andyh <andy.980f@gmail.com>
2025-01-21 12:34:07 -0600

    fix bug and propose alternate implementation.

--- now.cpp
+++ now.cpp
@@ -1,4 +1,3 @@
-
 /**
 // Created by andyh on 1/21/25.
 // Copyright (c) 2025 Andy Heilveil, (github/980f). All rights reserved.
@@ -23,51 +22,57 @@
 */
 
 int Now::monthFromAbbrev(const StringView &month) {
-  static const char *monthnames[]={"JAN", "FEB", "MAR", "APR", "MAY", "JUN","JUL","AUG","SEP","OCT","NOV","DEC"};
+  static const char *monthnames[] = {"JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"};
   return month.listIndex(monthnames,countof(monthnames));
 }
 
 int Now::dayFromAbbrev(const StringView &day) {
-  static const char * daynames[]={"Sun", "Mon", "Tue", "Wed", "Thu", "Fri","Sat",};
+  static const char *daynames[] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat",};
   return day.listIndex(daynames, countof(daynames));
 }
 
+/* todo: use strptime with each of the formats hoping to get one that returns a pointer to a null. */
 void Now::operator=(StringView timeString) {
   tm incoming;
-  memset(&incoming, 0, sizeof(incoming));//better than garbage, may not be legal in all fields.
-    while (timeString.length > 0) {
-      auto field=timeString.cutToken(' ',true);//need true for second argument to handle ANSI C asctime
-      Char firstChar=field[0];
-      if (firstChar.isDigit()) {
-        if (field.length==9) {//dd-mon-yy
-          incoming.tm_mday=atoi(field.cutToken('-',false));
-          auto month=field.cutToken('-',false);
-          incoming.tm_mon=monthFromAbbrev(month);
-          incoming.tm_year=atoi(field.begin());//spec is years since 1900.
-          continue;
-        }
-        if (field.length==8) {//must be hh:mm:ss
-          incoming.tm_hour=atoi(field.cutToken(':',false));
-          incoming.tm_min=atoi(field.cutToken(':',false));
-          incoming.tm_sec=atoi(field.begin());
-          continue;
-        }
-        if (field.length==4) {
-          incoming.tm_year=atoi(field.begin())-1900;
-          continue;
-        }
-        if (field.length<=2) {
-          incoming.tm_wday=atoi(field.begin());
-          continue;
+  memset(&incoming, 0, sizeof(incoming)); //better than garbage, may not be legal in all fields.
+  while (timeString.length > 0) {
+    auto field = timeString.cutToken(' ', true); //need true for second argument to handle ANSI C asctime
+    Char firstChar = field[0];
+    if (firstChar.isDigit()) {
+      if (field.length == 9) { //dd-mon-yy
+        incoming.tm_mday = atoi(field.cutToken('-', false));
+        auto month = field.cutToken('-', false);
+        incoming.tm_mon = monthFromAbbrev(month);
+        incoming.tm_year = atoi(field.begin()); //spec is years since 1900.
+        continue;
+      }
+      if (field.length == 8) { //must be hh:mm:ss
+        incoming.tm_hour = atoi(field.cutToken(':', false));
+        incoming.tm_min = atoi(field.cutToken(':', false));
+        incoming.tm_sec = atoi(field.begin());
+        continue;
+      }
+      if (field.length == 4) {
+        incoming.tm_year = atoi(field.begin()) - 1900;
+        continue;
+      }
+      if (field.length <= 2) {
+        incoming.tm_wday = atoi(field.begin());
+        continue;
+      }
+    } else {
+      //dow mon GMT
+      auto monthindex = monthFromAbbrev(field.begin());
+      if (monthindex == -1) { //not a month
+        auto dayindex = dayFromAbbrev(field.chop(3));
+        if (dayindex == -1) {
+          //hopeless or timezone
+        } else {
+          incoming.tm_mday = dayindex;
         }
       } else {
-        //dow mon GMT
-        auto monthindex=monthFromAbbrev(field.begin());
-        if (monthindex==-1) {//not a month
-          auto dayindex=dayFromAbbrev(field.chop(3));
-        } else {
-          incoming.tm_mon=monthindex;
-        }
+        incoming.tm_mon = monthindex;
       }
     }
+  }
 }
